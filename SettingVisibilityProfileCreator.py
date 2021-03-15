783c63804bbfb8969df5919f3280b06509477356
fieldOfView <aldo@fieldofview.com>
2021-03-15 12:21:08 +0100

    Update copyright information

--- SettingVisibilityProfileCreator.py
+++ SettingVisibilityProfileCreator.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019 fieldOfView
+# Copyright (c) 2021 Aldo Hoeben / fieldOfView
 # SettingVisibilityProfileCreator is released under the terms of the AGPLv3 or higher.
 
 from configparser import ConfigParser
